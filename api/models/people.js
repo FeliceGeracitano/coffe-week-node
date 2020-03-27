086198985979fee50388aa923f645e255785b25d
Felice <felice.geracitano@utmost.co>
2020-03-27 18:19:54 +0000

    progress model

--- api/models/people.js
+++ api/models/people.js
@@ -1,14 +1,48 @@
-module.exports = [
-  {
-    name: {
-      first: "Felice",
-      last: "Gercitano"
-    },
+const faker = require("faker");
+
+const dublinPeople = new Array(20).fill(undefined).map(_ => {
+  const first = faker.name.firstName();
+  const last = faker.name.lastName();
+  return {
+    name: { first, last },
     location: "dublin",
     department: "engineering",
-    motto: "Geronimo!",
-    email: "felice.geracitano@utmost.com",
-    phone: "08344545433",
-    uid: "d20c20ab-5813-48d4-8148-dee37530a1ec"
-  }
-];
+    motto: faker.lorem.sentence(6),
+    email: `${first.toLowerCase()}.${last.toLowerCase()}@utmost.co`,
+    phone: faker.phone.phoneNumberFormat(0),
+    uid: faker.random.uuid()
+  };
+});
+
+const sanFraPeople = new Array(10).fill(undefined).map(_ => {
+  const first = faker.name.firstName();
+  const last = faker.name.lastName();
+  return {
+    name: { first, last },
+    location: "san francisco",
+    department: "engineering",
+    motto: faker.lorem.sentence(6),
+    email: `${first.toLowerCase()}.${last.toLowerCase()}@utmost.co`,
+    phone: faker.phone.phoneNumberFormat(0),
+    uid: faker.random.uuid()
+  };
+});
+
+const nonEgPeople = new Array(5).fill(undefined).map(_ => {
+  const first = faker.name.firstName();
+  const last = faker.name.lastName();
+  return {
+    name: { first, last },
+    location: faker.address.city(),
+    department: faker.commerce.department,
+    motto: faker.lorem.sentence(6),
+    email: `${first.toLowerCase()}.${last.toLowerCase()}@utmost.co`,
+    phone: faker.phone.phoneNumberFormat(0),
+    uid: faker.random.uuid()
+  };
+});
+
+module.exports = []
+  .concat(dublinPeople)
+  .concat(sanFraPeople)
+  .concat(nonEgPeople);
