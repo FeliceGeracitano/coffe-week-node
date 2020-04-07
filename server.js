c6a9bc69ff541888aa44f5005069ac82a87f7bd0
Felice <felice.geracitano@utmost.co>
2020-04-07 12:46:59 +0100

    progress

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require("express"),
+  cors = require("cors"),
   app = express(),
   port = process.env.PORT || 3000,
   bodyParser = require("body-parser");
 
+app.use(cors());
 app.use(bodyParser.json());
 routes = require("./api/routes/people");
 routes(app);
